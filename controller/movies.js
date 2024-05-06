32454a9d63185c57d453b7a7e727b2561d693f1d
jbui184 <johnnybui184@gmail.com>
2024-05-06 19:56:49 +1000

    first commit

---
+++ controller/movies.js
@@ -0,0 +1,33 @@
+const Movie = require("../models/movie");
+
+module.exports = {
+  new: newMovie,
+  create,
+  index,
+};
+
+function newMovie(req, res) {
+  res.render("movies/new", { errorMsg: "" });
+}
+
+async function create(req, res) {
+  req.body.nowShowing = !!req.body.nowShowing;
+
+  req.body.cast = req.body.cast.trim();
+
+  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  try {
+    await Movie.create(req.body);
+
+    res.redirect("/movies");
+  } catch (err) {
+    console.log(err);
+    res.render("movies/new", { errorMsg: err.message });
+  }
+}
+
+async function index(req, res) {
+  res.render("movies", {
+    movies: await Movie.find({}),
+  });
+}
